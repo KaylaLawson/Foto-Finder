17c00a9b0cef5b4e3635f9f5deea59df9926ce59
Kayla Lawson <kaylarae7128@gmail.com>
2019-01-05 15:41:29 -0700

    Start on the js keep working through small sprints

--- main.js
+++ main.js
@@ -1,10 +1,32 @@
- // Notes from class for creat function
+var createAlbum = document.querySelector('.add-to-album-btn');
+var photoFile = document.querySelector('.choose-file-btn');
+var photoGallery = document.querySelector('.images');
+var imagesArr = JSON.parse(localStorage.getItem('photos')) || [];
+var reader = new FileReader();
 
+window.addEventListener('load', appendPhotos);
+createAlbum.addEventListener('click', albumCard);
 
-function createElement() {
-    console.log(input.files[0])
-  if(input.files[0]) {
-    reader.readAsDataURL(input.files[0]);
-    // reader.onload = addPhoto
+function appendPhotos() {
+  imagesArr.forEach(function (photo) {
+    photoGallery.innerHTML += `<img src=${photo.file} />`
+  })
+}
+
+function albumCard(event) {
+  event.preventDefault();
+  if (photoFile.files[0]) {
+    reader.readAsDataURL(photoFile.files[0]); 
+    reader.onload = addPhoto
   }
 }
+
+function addPhoto(e) {
+  // console.log(e.target.result);
+  var newPhoto = new Photo(Date.now(), e.target.result);
+  photoGallery.innerHTML += `<img src=${e.target.result} />`;
+  imagesArr.push(newPhoto)
+  newPhoto.saveToStorage(imagesArr)
+}
+
+var photoInput = document.querySelector('.choose-file-btn');
--- photo.js
+++ photo.js
@@ -1,6 +1,9 @@
-class photo {
-  constructor() {
-
+class Photo {
+  constructor(id, title, caption, file, favorite) {
+    this.id = Date.now();
+    this.title = title;
+    this.file =file;
+    this.favorite = favorite || false;
   }
 
   saveToStorage() {
