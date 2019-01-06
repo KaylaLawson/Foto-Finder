f5fac3790f27ca5f937c9c3bd13e1303f8a2695a
Kayla Lawson <kaylarae7128@gmail.com>
2019-01-06 14:23:47 -0700

    Work on organizing current functions

--- main.js
+++ main.js
@@ -43,35 +43,12 @@ function addPhoto(e) {
   populateCard(newPhoto.photoId, newPhoto.file, newPhoto.title, newPhoto.caption);
   imagesArr.push(newPhoto);
   newPhoto.saveToStorage(imagesArr);
+  title.value = "";
+  caption.value = "";
 }
-  
-  // add favoiret later
-  // photoGallery.innerHTML += 
-  // `
-  // <section class="foto-card-container">
-  //   <h2 class="foto-title" contenteditable="true">${title.value}</h2>
-  //   <article class="foto-image"><img src=${e.target.result} /></article>
-  //   <h2 class="foto-caption" contenteditable="true">${caption.value}</h2>
-  //   <article class="foto-card-footer">
-  //     <img class="trash-icon" src="assets/delete.svg">
-  //     <img calss="heart-icon" src="assets/favorite.svg">
-  //   </article>
-  // </section>
-  // `;
-
 
 function appendPhotos() {
   imagesArr.forEach(function (photo) {
     populateCard(photo.id, photo.file, photo.title, photo.caption);
     });
 }
-    // photoGallery.innerHTML += 
-      // `<section class="foto-card-container">
-      //   <h2 class="foto-title" contenteditable="true">${photo.title}</h2>
-      //   <article class="foto-image"><img src=${photo.file} /></article>
-      //   <h2 class="foto-caption" contenteditable="true">${photo.caption}</h2>
-      //   <article class="foto-card-footer">
-      //     <img class="trash-icon" src="assets/delete.svg">
-      //     <img calss="heart-icon" src="assets/favorite.svg">
-      //   </article>
-      // </section>`;
